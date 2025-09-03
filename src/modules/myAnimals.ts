940a3379c2d80a00f53bcc6face89a9938f15e80
Jesper Olsson <jesper.olsson.vxo@gmail.com>
2025-09-03 10:56:16 +0200

    removed js linting

--- src/modules/myAnimals.ts
+++ src/modules/myAnimals.ts
@@ -3,14 +3,14 @@ import { Animal } from './animal'
 const Dog1 = new Animal ('Brutus', 28, 'Dog', true)
 
 console.log (Dog1)
-Dog1.makeSound() // --- IGNORE ---
+Dog1.makeSound()
 Dog1.info()
 Dog1.markTerritory()
 
 const Dog2 = new Animal ('Fido', 3, 'Dog', false)
 
 console.log (Dog2);
-Dog2.makeSound() // --- IGNORE ---
+Dog2.makeSound()
 Dog2.info()  
 
 Dog1.name = 'Booooogers'
